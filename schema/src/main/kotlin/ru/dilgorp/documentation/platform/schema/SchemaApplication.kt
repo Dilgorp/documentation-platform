24af1314c594f2ca4eeadefcfa1e3d27ba69312e
kvcherepanov <koncherepanov@yandex.ru>
2023-10-03 20:13:49 +0500

    schema service was added
    Signed-off-by: kvcherepanov <koncherepanov@yandex.ru>

---
+++ schema/src/main/kotlin/ru/dilgorp/documentation/platform/schema/SchemaApplication.kt
@@ -0,0 +1,11 @@
+package ru.dilgorp.documentation.platform.schema
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShemaApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShemaApplication>(*args)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 include(
-        "gateway"
+        "gateway",
+        "schema"
 )
