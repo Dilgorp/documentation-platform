7eb2941fb768a5a8434df478377cbdd6ee45d180
kvcherepanov <koncherepanov@yandex.ru>
2023-10-16 16:01:48 +0500

    template module was added
    Signed-off-by: kvcherepanov <koncherepanov@yandex.ru>

---
+++ template/src/main/kotlin/ru/dilgorp/documentation/platform/template/TemplateApplication.kt
@@ -0,0 +1,11 @@
+package ru.dilgorp.documentation.platform.schema
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TempltaeApplication
+
+fun main(args: Array<String>) {
+    runApplication<TempltaeApplication>(*args)
+}
